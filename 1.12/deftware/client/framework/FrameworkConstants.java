7b5b91a025e2cb02c5716031e3b48ca4fdcd5029
VTCAKAVSMoACE <vtcakavsmoace@gmail.com>
2017-06-14 14:38:49 +0100

    Split branch 1.11.2

--- 1.12/deftware/client/framework/FrameworkConstants.java
+++
@@ -1,9 +0,0 @@
-package me.deftware.client.framework;
-
-public class FrameworkConstants {
-
-	public static final int VERSION = 3;
-	public static final String AUTHOR = "Deftware";
-	public static final String FRAMEWORK_NAME = "EMC";
-	
-}
