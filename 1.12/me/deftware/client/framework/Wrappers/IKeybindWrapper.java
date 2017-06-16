e1a05ea0d8cc21e480feaf64ecf273820fcfe025
Deftware <strumaticjoker2@gmail.com>
2017-06-16 22:34:59 +0200

    Add more keys to IKeybindWrapper

--- 1.12/me/deftware/client/framework/Wrappers/IKeybindWrapper.java
+++ 1.12/me/deftware/client/framework/Wrappers/IKeybindWrapper.java
@@ -1,5 +1,7 @@
 package me.deftware.client.framework.Wrappers;
 
+import org.lwjgl.input.Keyboard;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
 
@@ -9,11 +11,24 @@ public class IKeybindWrapper {
 		return bind.bind.pressed;
 	}
 	
+	public static void setPressed(IKeybind bind, boolean state) {
+		bind.bind.pressed = state;
+	}
+
+	public static boolean isKeyDown(IKeybind bind) {
+		return Keyboard.isKeyDown(bind.bind.getKeyCode());
+	}
+
 	public static enum IKeybind {
 		
 		SNEAK(Minecraft.getMinecraft().gameSettings.keyBindSneak), USEITEM(
 				Minecraft.getMinecraft().gameSettings.keyBindUseItem), JUMP(
-						Minecraft.getMinecraft().gameSettings.keyBindJump);
+						Minecraft.getMinecraft().gameSettings.keyBindJump), SPRINT(
+								Minecraft.getMinecraft().gameSettings.keyBindSprint), FORWARD(
+										Minecraft.getMinecraft().gameSettings.keyBindForward), BACK(
+												Minecraft.getMinecraft().gameSettings.keyBindBack), LEFT(
+														Minecraft.getMinecraft().gameSettings.keyBindLeft), RIGHT(
+																Minecraft.getMinecraft().gameSettings.keyBindRight);
 
 		KeyBinding bind;
 
