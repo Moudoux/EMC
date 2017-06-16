aae99ddce72044c39a37bf136200a5b0245f2e5b
Deftware <strumaticjoker2@gmail.com>
2017-06-16 15:58:32 +0200

    Add IGuiTextField position wrappers

--- 1.12/me/deftware/client/framework/Wrappers/Objects/Text/IGuiTextField.java
+++ 1.12/me/deftware/client/framework/Wrappers/Objects/Text/IGuiTextField.java
@@ -8,7 +8,7 @@ public class IGuiTextField extends GuiTextField {
 	public IGuiTextField(int componentId, int x, int y, int width, int height) {
 		super(componentId, Minecraft.getMinecraft().fontRendererObj, x, y, width, height);
 	}
-	
+
 	public String getTextboxText() {
 		return this.getText();
 	}
@@ -25,10 +25,6 @@ public class IGuiTextField extends GuiTextField {
 		this.setFocused(state);
 	}
 	
-	/*
-	 * Wrappers
-	 */
-	
 	public void onKeyTyped(char typedChar, int keyCode) {
 		this.textboxKeyTyped(typedChar, keyCode);
 	}
@@ -43,6 +39,26 @@ public class IGuiTextField extends GuiTextField {
 	
 	public void doCursorTick() {
 		this.updateCursorCounter();
+	}
+
+	public int getPosX() {
+		return this.xPosition;
+	}
+
+	public int getPosY() {
+		return this.yPosition;
+	}
+
+	public void setPosX(int x) {
+		this.xPosition = x;
+	}
+
+	public void setPosY(int y) {
+		this.yPosition = y;
+	}
+
+	public void setIEnableBackgroundDrawing(boolean state) {
+		this.setEnableBackgroundDrawing(state);
 	}
 
 }
