759cb4e98b53e30ed03e80fdf4921c6d2072c9ad
Moudoux <strumaticjoker2@gmail.com>
2017-07-22 17:23:44 +0200

    Add ITeam

--- 1.12/me/deftware/client/framework/Event/Events/EventBlockhardness.java
+++ 1.12/me/deftware/client/framework/Event/Events/EventBlockhardness.java
@@ -1,42 +1,21 @@
 package me.deftware.client.framework.Event.Events;
 
 import me.deftware.client.framework.Event.Event;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
 public class EventBlockhardness extends Event {
 
-	private IBlockState state;
-	private EntityPlayer player;
-	private World world;
-	private BlockPos pos;
+	private float multiplier;
 
-	private float hardness;
-
-	public EventBlockhardness(IBlockState state, EntityPlayer player, World worldIn, BlockPos pos, float hardness) {
-		this.state = state;
-		this.player = player;
-		this.world = worldIn;
-		this.pos = pos;
-		this.hardness = hardness;
+	public EventBlockhardness() {
+		this.multiplier = 1;
 	}
 
-	public boolean canHarvest() {
-		return player.canHarvestBlock(state);
+	public float getMultiplier() {
+		return multiplier;
 	}
 
-	public float getDigSpeed() {
-		return player.getDigSpeed(state);
-	}
-
-	public float getHardness() {
-		return hardness;
-	}
-
-	public void setHardness(float hardness) {
-		this.hardness = hardness;
+	public void setMultiplier(float multiplier) {
+		this.multiplier = multiplier;
 	}
 
 }
