6ff9cacd6ea9efb8b97088c049c4e8da83148683
Deftware <strumaticjoker2@gmail.com>
2017-10-20 21:46:50 +0200

    Add new ICPacketRotation constructor

--- src/me/deftware/client/framework/Event/Events/Packet/Packets/ICPacketRotation.java
+++ src/me/deftware/client/framework/Event/Events/Packet/Packets/ICPacketRotation.java
@@ -2,11 +2,16 @@ package me.deftware.client.framework.Event.Events.Packet.Packets;
 
 import me.deftware.client.framework.Event.Events.Packet.IPacket;
 import net.minecraft.network.Packet;
+import net.minecraft.network.play.client.CPacketPlayer;
 
 public class ICPacketRotation extends IPacket {
 
 	public ICPacketRotation(Packet<?> packet) {
 		super(packet);
+	}
+
+	public ICPacketRotation(float yaw, float pitch, boolean onGround) {
+		super(new CPacketPlayer.Rotation(yaw, pitch, onGround));
 	}
 
 }
