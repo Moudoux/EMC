6d8275b7a95430be65c91d13ec55a7747d1d4ab9
ThatGamerBlue <thatgamerblue@gmail.com>
2018-10-17 18:57:15 +0100

    Add hostile mobs

--- src/main/java/me/deftware/client/framework/wrappers/entity/IEntity.java
+++ src/main/java/me/deftware/client/framework/wrappers/entity/IEntity.java
@@ -5,7 +5,7 @@ import net.minecraft.client.entity.EntityOtherPlayerMP;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.*;
 import net.minecraft.entity.EntityFlying;
-import net.minecraft.entity.boss.EntityWither;
+import net.minecraft.entity.boss.*;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.monster.*;
 import net.minecraft.entity.passive.*;
@@ -201,6 +201,8 @@ public class IEntity {
 			return entity instanceof EntityLiving;
 		} else if (e.equals(EntityType.ENTITY_ITEM)) {
 			return entity instanceof EntityItem;
+		} else if (e.equals(EntityType.ENTITY_PROJECTILE)) {
+			return entity instanceof IProjectile;
 		}
 		// Mobs
 		if (e.equals(EntityType.ENTITY_WOLF)) {
@@ -213,14 +215,12 @@ public class IEntity {
 			return entity instanceof EntityWaterMob;
 		} else if (e.equals(EntityType.EntityMob)) {
 			return entity instanceof EntityMob;
-		} else if (e.equals(EntityType.EntitySlime)) {
+		} else if (e.equals(EntityType.EntitySlime) || e.equals(EntityType.ENTITY_SLIME)) {
 			return entity instanceof EntitySlime;
 		} else if (e.equals(EntityType.EntityFlying)) {
 			return entity instanceof EntityFlying;
 		} else if (e.equals(EntityType.EntityGolem)) {
 			return entity instanceof EntityGolem;
-		} else if (e.equals(EntityType.ENTITY_SPIDER)) {
-			return entity instanceof EntitySpider;
 		} else if (e.equals(EntityType.ENTITY_SPIDER)) {
 			return entity instanceof EntitySpider;
 		} else if (e.equals(EntityType.ENTITY_ZOMBIE_PIGMAN)) {
@@ -229,16 +229,68 @@ public class IEntity {
 			return entity instanceof EntityEnderman;
 		} else if (e.equals(EntityType.ENTITY_WITHER_SKELETON)) {
 		    return entity instanceof EntityWitherSkeleton;
+		} else if (e.equals(EntityType.ENTITY_WITHER)) {
+			return entity instanceof EntityWither;
+		} else if (e.equals(EntityType.ENTITY_DRAGON)) {
+			return entity instanceof EntityDragon;
+		} else if (e.equals(EntityType.ENTITY_PHANTOM)) {
+			return entity instanceof EntityPhantom;
+		} else if (e.equals(EntityType.ENTITY_DROWNED)) {
+			return entity instanceof EntityDrowned;
+		} else if (e.equals(EntityType.ENTITY_EVOKER)) {
+			return entity instanceof EntityEvoker;
+		} else if (e.equals(EntityType.ENTITY_STRAY)) {
+			return entity instanceof EntityStray;
+		} else if (e.equals(EntityType.ENTITY_ELDER_GUARDIAN)) {
+			return entity instanceof EntityElderGuardian;
+		} else if (e.equals(EntityType.ENTITY_CREEPER)) {
+			return entity instanceof EntityCreeper;
+		} else if (e.equals(EntityType.ENTITY_VINDICATOR)) {
+			return entity instanceof EntityVindicator;
+		} else if (e.equals(EntityType.ENTITY_ILLUSIONER)) {
+			return entity instanceof EntityIllusionIllager;
+		} else if (e.equals(EntityType.ENTITY_HUSK)) {
+			return entity instanceof EntityHusk;
+		} else if (e.equals(EntityType.ENTITY_ZOMBIE)) {
+			return entity instanceof EntityZombie;
+		} else if (e.equals(EntityType.ENTITY_SKELETON)) {
+			return entity instanceof EntitySkeleton;
+		} else if (e.equals(EntityType.ENTITY_SHULKER)) {
+			return entity instanceof EntityShulker;
+		} else if (e.equals(EntityType.ENTITY_GUARDIAN)) {
+			return entity instanceof EntityGuardian;
+		} else if (e.equals(EntityType.ENTITY_VEX)) {
+			return entity instanceof EntityVex;
+		} else if (e.equals(EntityType.ENTITY_SILVERFISH)) {
+			return entity instanceof EntitySilverfish;
+		} else if (e.equals(EntityType.ENTITY_WITCH)) {
+			return entity instanceof EntityWitch;
+		} else if (e.equals(EntityType.ENTITY_GIANT)) {
+			return entity instanceof EntityGiantZombie;
+		} else if (e.equals(EntityType.ENTITY_BLAZE)) {
+			return entity instanceof EntityBlaze;
+		} else if (e.equals(EntityType.ENTITY_ENDERMITE)) {
+			return entity instanceof EntityEndermite;
+		} else if (e.equals(EntityType.ENTITY_GHAST)) {
+			return entity instanceof EntityGhast;
+		} else if (e.equals(EntityType.ENTITY_MAGMA_CUBE)) {
+			return entity instanceof EntityMagmaCube;
+		} else if (e.equals(EntityType.ENTITY_CAVE_SPIDER)) {
+			return entity instanceof EntityCaveSpider;
 		}
 		return false;
 	}
 
 	public enum EntityType {
-		ENTITY_PLAYER_SP, EntityOtherPlayerMP, ENTITY_PLAYER, EntitySlime, EntityGolem, EntityFlying, EntityMob, EntityWaterMob, ENTITY_WOLF, ENTITY_LIVING_BASE, ENTITY_LIVING, Entity_Ageable, EntityAmbientCreature, ENTITY_ITEM,
+		ENTITY_PLAYER_SP, EntityOtherPlayerMP, ENTITY_PLAYER, EntitySlime, EntityGolem, EntityFlying, EntityMob, EntityWaterMob, ENTITY_WOLF,
+		ENTITY_LIVING_BASE, ENTITY_LIVING, Entity_Ageable, EntityAmbientCreature, ENTITY_ITEM, ENTITY_PROJECTILE,
 		/*
 		 * Hostile mobs
 		 */
-		ENTITY_ENDERMAN, ENTITY_ZOMBIE_PIGMAN, ENTITY_SPIDER, ENTITY_WITHER_SKELETON
+		ENTITY_ENDERMAN, ENTITY_ZOMBIE_PIGMAN, ENTITY_SPIDER, ENTITY_WITHER_SKELETON, ENTITY_WITHER, ENTITY_DRAGON, ENTITY_PHANTOM, ENTITY_DROWNED,
+		ENTITY_EVOKER, ENTITY_STRAY, ENTITY_ELDER_GUARDIAN, ENTITY_CREEPER, ENTITY_VINDICATOR, ENTITY_ILLUSIONER, ENTITY_ZOMBIE, ENTITY_HUSK,
+		ENTITY_SKELETON, ENTITY_SHULKER, ENTITY_SLIME, ENTITY_GUARDIAN, ENTITY_VEX, ENTITY_SILVERFISH, ENTITY_WITCH, ENTITY_GIANT, ENTITY_BLAZE,
+		ENTITY_ENDERMITE, ENTITY_GHAST, ENTITY_MAGMA_CUBE, ENTITY_CAVE_SPIDER
 	}
 
 }
