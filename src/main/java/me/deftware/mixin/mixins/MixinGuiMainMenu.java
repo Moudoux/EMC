b21cd88a507fbb972ea3a691ddcb37ee65449ca3
deftware <strumaticjoker2@gmail.com>
2019-02-16 16:24:10 +0100

    Update to EMC 13.7.6

--- src/main/java/me/deftware/mixin/mixins/MixinGuiMainMenu.java
+++ src/main/java/me/deftware/mixin/mixins/MixinGuiMainMenu.java
@@ -14,7 +14,7 @@ public class MixinGuiMainMenu {
     @Inject(method = "draw", at = @At("RETURN"))
     public void draw(int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
         MinecraftClient.getInstance().fontRenderer.drawWithShadow(
-                FrameworkConstants.FRAMEWORK_NAME + " v" + FrameworkConstants.VERSION, 2, 2, 0xFFFFFF);
+                FrameworkConstants.FRAMEWORK_NAME + " v" + FrameworkConstants.VERSION + "." + FrameworkConstants.PATCH, 2, 2, 0xFFFFFF);
     }
 
 }
