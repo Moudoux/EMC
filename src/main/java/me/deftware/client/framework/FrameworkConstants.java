2478bcadccc4de567c41ed411b4fdc5d34b721d6
deftware <strumaticjoker2@gmail.com>
2019-02-16 15:23:25 +0100

    Add MappingSystem to FrameworkConstants

--- src/main/java/me/deftware/client/framework/FrameworkConstants.java
+++ src/main/java/me/deftware/client/framework/FrameworkConstants.java
@@ -2,11 +2,19 @@ package me.deftware.client.framework;
 
 public class FrameworkConstants {
 
-	public static double VERSION = 13.7;
-	public static int PATCH = 5;
-	public static boolean FORGE_MODE = false;
+    public static double VERSION = 13.7;
+    public static int PATCH = 4;
 
-	public static String AUTHOR = "Deftware";
-	public static String FRAMEWORK_NAME = "EMC";
+    public static String AUTHOR = "Deftware";
+    public static String FRAMEWORK_NAME = "EMC";
+    public static MappingSystem MAPPING_SYSTEM = MappingSystem.MCP;
+
+	/*
+		Mod info
+	 */
+
+    public static enum MappingSystem {
+        Yarn, MCP
+    }
 
 }
