bd221c01a465b1781b5d56de17011a9fe0582126
deftware <strumaticjoker2@gmail.com>
2019-02-21 15:12:47 +0100

    Add EventSlowdown

--- src/main/java/me/deftware/mixin/mixins/MixinMovementInputFromOptions.java
+++ src/main/java/me/deftware/mixin/mixins/MixinMovementInputFromOptions.java
@@ -1,17 +1,62 @@
 package me.deftware.mixin.mixins;
 
 import me.deftware.client.framework.event.events.EventSlowdown;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.input.KeyboardInput;
+import net.minecraft.client.options.GameOptions;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 
-import static org.spongepowered.asm.lib.Opcodes.GETFIELD;
-
+@SuppressWarnings("ALL")
 @Mixin(KeyboardInput.class)
 public class MixinMovementInputFromOptions {
 
+    @Shadow
+    @Final
+    public GameOptions settings;
 
+    @Overwrite
+    public void tick() {
+        ((KeyboardInput) (Object) this).field_3907 = 0.0F;
+        ((KeyboardInput) (Object) this).field_3905 = 0.0F;
+        if (this.settings.keyForward.isPressed()) {
+            ++((KeyboardInput) (Object) this).field_3905;
+            ((KeyboardInput) (Object) this).forward = true;
+        } else {
+            ((KeyboardInput) (Object) this).forward = false;
+        }
+
+        if (this.settings.keyBack.isPressed()) {
+            --((KeyboardInput) (Object) this).field_3905;
+            ((KeyboardInput) (Object) this).back = true;
+        } else {
+            ((KeyboardInput) (Object) this).back = false;
+        }
+
+        if (this.settings.keyLeft.isPressed()) {
+            ++((KeyboardInput) (Object) this).field_3907;
+            ((KeyboardInput) (Object) this).left = true;
+        } else {
+            ((KeyboardInput) (Object) this).left = false;
+        }
+
+        if (this.settings.keyRight.isPressed()) {
+            --((KeyboardInput) (Object) this).field_3907;
+            ((KeyboardInput) (Object) this).right = true;
+        } else {
+            ((KeyboardInput) (Object) this).right = false;
+        }
+
+        ((KeyboardInput) (Object) this).jumping = this.settings.keyJump.isPressed();
+        ((KeyboardInput) (Object) this).sneaking = this.settings.keySneak.isPressed();
+
+        EventSlowdown event = new EventSlowdown(EventSlowdown.SlowdownType.Sneak).send();
+
+        if (((KeyboardInput) (Object) this).sneaking && !event.isCanceled()) {
+            ((KeyboardInput) (Object) this).field_3907 = (float) ((double) ((KeyboardInput) (Object) this).field_3907 * 0.3D);
+            ((KeyboardInput) (Object) this).field_3905 = (float) ((double) ((KeyboardInput) (Object) this).field_3905 * 0.3D);
+        }
+    }
 
 }
