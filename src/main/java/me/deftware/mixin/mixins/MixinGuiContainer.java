02f14acc97320443879d81a587bffb320fbbe89c
deftware <strumaticjoker2@gmail.com>
2019-04-03 19:27:55 +0200

    Move render event to GuiContainer

--- src/main/java/me/deftware/mixin/mixins/MixinGuiContainer.java
+++ src/main/java/me/deftware/mixin/mixins/MixinGuiContainer.java
@@ -1,13 +1,18 @@
 package me.deftware.mixin.mixins;
 
+import me.deftware.client.framework.event.events.EventGuiScreenPostDraw;
 import me.deftware.mixin.imp.IMixinGuiContainer;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.inventory.Slot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiContainer.class)
-public class MixinGuiContainer implements IMixinGuiContainer {
+public class MixinGuiContainer extends MixinGuiScreen implements IMixinGuiContainer {
 
     @Shadow
     private Slot hoveredSlot;
@@ -15,6 +20,16 @@ public class MixinGuiContainer implements IMixinGuiContainer {
     @Override
     public Slot getHoveredSlot() {
         return hoveredSlot;
+    }
+
+    @Inject(method = "<init>*", at = @At("RETURN"))
+    private void onConstructed(CallbackInfo ci) {
+        this.shouldSendPostRenderEvent = false;
+    }
+
+    @Inject(method = "render", at = @At("RETURN"), remap = false)
+    public void render_return(int x, int y, float p_render_3_, CallbackInfo ci) {
+        new EventGuiScreenPostDraw((GuiScreen) (Object) this, x, y).broadcast();
     }
 
 }
