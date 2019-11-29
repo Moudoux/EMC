650618a6d446eec7e45d983d45cdc5acc88685e0
CDAGaming <cstack2011@yahoo.com>
2019-11-29 11:14:24 -0600

    Update to 1.15-pre3

--- src/main/java/me/deftware/mixin/mixins/MixinVisGraph.java
+++ src/main/java/me/deftware/mixin/mixins/MixinVisGraph.java
@@ -1,14 +1,14 @@
 package me.deftware.mixin.mixins;
 
 import me.deftware.client.framework.maps.SettingsMap;
-import net.minecraft.client.render.chunk.ChunkOcclusionGraphBuilder;
+import net.minecraft.client.render.chunk.ChunkOcclusionDataBuilder;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ChunkOcclusionGraphBuilder.class)
+@Mixin(ChunkOcclusionDataBuilder.class)
 public class MixinVisGraph {
 
     @Inject(method = "markClosed", at = @At("HEAD"), cancellable = true)
