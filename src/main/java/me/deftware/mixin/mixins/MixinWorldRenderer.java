062430f7c054cd4045d6a1ad950b3153bacbeb60
CDAGaming <cstack2011@yahoo.com>
2019-12-18 16:29:02 -0600

    [Fix] Fix EventWeather not triggering properly

--- src/main/java/me/deftware/mixin/mixins/MixinWorldRenderer.java
+++ src/main/java/me/deftware/mixin/mixins/MixinWorldRenderer.java
@@ -2,6 +2,7 @@ package me.deftware.mixin.mixins;
 
 import me.deftware.client.framework.event.events.EventWeather;
 import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.render.WorldRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,6 +14,15 @@ public class MixinWorldRenderer {
 
     @Inject(method = "method_22713", at = @At("HEAD"), cancellable = true)
     private void renderRain(Camera camera, CallbackInfo ci) {
+        EventWeather event = new EventWeather(EventWeather.WeatherType.Rain);
+        event.broadcast();
+        if (event.isCanceled()) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "renderWeather", at = @At("HEAD"), cancellable = true)
+    private void renderWeather(LightmapTextureManager manager, float f, double d, double e, double g, CallbackInfo ci) {
         EventWeather event = new EventWeather(EventWeather.WeatherType.Rain);
         event.broadcast();
         if (event.isCanceled()) {
