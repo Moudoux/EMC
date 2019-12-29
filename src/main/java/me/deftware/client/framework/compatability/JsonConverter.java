697fad7dd80082fb504c7591fe31ce7630748b72
deftware <strumaticjoker2@gmail.com>
2019-12-29 19:57:18 +0100

    Add fallback

--- src/main/java/me/deftware/client/framework/compatability/JsonConverter.java
+++ src/main/java/me/deftware/client/framework/compatability/JsonConverter.java
@@ -1,6 +1,9 @@
 package me.deftware.client.framework.compatability;
 
 import com.google.gson.*;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.stage.FileChooser;
 import me.deftware.client.framework.main.bootstrap.Bootstrap;
 import me.deftware.client.framework.path.OSUtils;
 import net.minecraft.client.MinecraftClient;
@@ -12,8 +15,11 @@ import java.nio.charset.StandardCharsets;
 
 public class JsonConverter {
 
+    private static String manualJsonLocation;
+    private static boolean isFileDialogOpen = false;
+
     public JsonConverter() {
-        File jsonFile = new File(getRunningFolder() + getVersion() + ".json");
+        File jsonFile = getEMCJsonFile();
         if (!jsonFile.exists()) {
             Bootstrap.logger.error("Could not find Json file, is this a custom launcher?");
             return;
@@ -46,7 +52,6 @@ public class JsonConverter {
                 }
             }
             json.addProperty("mainClass", "me.deftware.client.framework.main.Main");
-            json.remove("emcMods");
             PrintWriter writer = new PrintWriter(jsonFile.getAbsolutePath(), "UTF-8");
             writer.println(json.toString());
             writer.close();
@@ -55,6 +60,63 @@ public class JsonConverter {
             ex.printStackTrace();
             Bootstrap.logger.error("Could not convert Json file {}", jsonFile.getAbsolutePath());
         }
+    }
+
+    public static File getEMCJsonFile() {
+        String defaultJsonName = getRunningFolder() + getVersion() + ".json";
+        File jsonFile = new File(Bootstrap.EMCSettings != null ? Bootstrap.EMCSettings.getString("EMC_JSON_LOCATION", defaultJsonName) : defaultJsonName);
+
+        if (!jsonFile.exists()) {
+            if (manualJsonLocation != null && new File(manualJsonLocation).exists()) {
+                return new File(manualJsonLocation);
+            } else {
+                System.out.println("Opening File Open Dialog, as JSON Cannot be found...");
+
+                try {
+                    JFXPanel frame = new JFXPanel(); // Initialize JavaFX Environment
+                    isFileDialogOpen = true;
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            FileChooser fileChooser = new FileChooser();
+                            fileChooser.setTitle("Open EMC Json File (Required)");
+                            fileChooser.getExtensionFilters().addAll(
+                                    new FileChooser.ExtensionFilter("Json", "*.json")
+                            );
+
+                            File resultFile = fileChooser.showOpenDialog(null);
+
+                            if (resultFile != null) {
+                                manualJsonLocation = resultFile.getAbsolutePath();
+                            } else {
+                                System.out.println("JSON not found, things will break if other addons are using this!");
+                            }
+                            isFileDialogOpen = false;
+                        }
+                    });
+                } catch (Exception | Error ex) {
+                    System.out.println("Error: EMC Json File Open Dialog failed to open, please Input your EMC Json Location manually in your EMC Config @ emcJsonLocation");
+                    ex.printStackTrace();
+                }
+            }
+        } else {
+            return jsonFile;
+        }
+
+        while (isFileDialogOpen) {
+            try {
+                Thread.sleep(1000);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        // Save as New Json Location if Settings Available
+        if (Bootstrap.EMCSettings != null) {
+            Bootstrap.EMCSettings.saveString("EMC_JSON_LOCATION", manualJsonLocation);
+            Bootstrap.EMCSettings.saveConfig();
+        }
+        return new File(manualJsonLocation);
     }
 
     public static String getVersion() {
