e3a8e20c85a32f34e3bc500ed7520d9562684c7c
CDAGaming <cstack2011@yahoo.com>
2020-01-07 12:23:06 -0600

    [Fix] Restore this Mixin for RainbowEnchant

--- src/main/java/me/deftware/mixin/mixins/MixinTextureManager.java
+++ src/main/java/me/deftware/mixin/mixins/MixinTextureManager.java
@@ -1,19 +1,15 @@
 package me.deftware.mixin.mixins;
 
 import me.deftware.client.framework.maps.SettingsMap;
-import net.minecraft.client.texture.AbstractTexture;
-import net.minecraft.client.texture.ResourceTexture;
 import net.minecraft.client.texture.TextureManager;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(TextureManager.class)
 public class MixinTextureManager {
 
-    /*
     @ModifyVariable(method = "bindTextureInner", at = @At("HEAD"))
     private Identifier bindTexture(Identifier resource) {
         if ((boolean) SettingsMap.getValue(SettingsMap.MapKeys.RENDER, "RAINBOW_ITEM_GLINT", false)) {
@@ -23,6 +19,6 @@ public class MixinTextureManager {
             }
         }
         return resource;
-    }*/
+    }
 
 }
