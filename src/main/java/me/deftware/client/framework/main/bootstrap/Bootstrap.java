c3d35c0c4c6e1b0dc5a41877a3d57eaa4e2bc45a
CDAGaming <cstack2011@yahoo.com>
2020-01-14 18:46:15 -0600

    [Change] Added IGNORE_SHA1
    To allow debugging in runtime again

--- src/main/java/me/deftware/client/framework/main/bootstrap/Bootstrap.java
+++ src/main/java/me/deftware/client/framework/main/bootstrap/Bootstrap.java
@@ -29,7 +29,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Bootstrap {
 
     public static int CRASH_COUNT = 0;
-    public static boolean initialized = false, CRASHED = false;
+    public static boolean initialized = false, CRASHED = false, IGNORE_SHA1 = false;
     public static Logger logger = LogManager.getLogger(String.format("EMC v%s.%s", FrameworkConstants.VERSION, FrameworkConstants.PATCH));
     public static ArrayList<JsonObject> modsInfo = new ArrayList<>();
     public static boolean isRunning = true;
