a9906fd2168c23f72cfdb15c01d259e5a0707474
CDAGaming <cstack2011@yahoo.com>
2020-01-31 10:12:16 -0600

    [Change] Add means to manually edit packets within IPacket

--- src/main/java/me/deftware/client/framework/network/IPacket.java
+++ src/main/java/me/deftware/client/framework/network/IPacket.java
@@ -3,12 +3,16 @@ package me.deftware.client.framework.network;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.network.Packet;
 
+import java.io.IOException;
+
 /**
  * Describes the packet structure with all of it's data
  */
 public class IPacket {
 
     private Packet<?> packet;
+
+    private IPacketBuffer packetBuffer;
 
     public IPacket(Packet<?> packet) {
         this.packet = packet;
@@ -18,6 +22,13 @@ public class IPacket {
         return packet;
     }
 
+    public IPacketBuffer getPacketBuffer() {
+        return packetBuffer;
+    }
+
+    public void setPacketBuffer(IPacketBuffer buffer) throws IOException {
+        packet.write(buffer.buffer);
+    }
 
     public void sendPacket() {
         MinecraftClient.getInstance().player.networkHandler.sendPacket(packet);
--- src/main/java/me/deftware/client/framework/network/IPacketBuffer.java
+++ src/main/java/me/deftware/client/framework/network/IPacketBuffer.java
@@ -2,6 +2,7 @@ package me.deftware.client.framework.network;
 
 import io.netty.buffer.Unpooled;
 import me.deftware.client.framework.wrappers.item.IItemStack;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.PacketByteBuf;
 
 public class IPacketBuffer {
@@ -12,8 +13,48 @@ public class IPacketBuffer {
         this.buffer = new PacketByteBuf(Unpooled.buffer());
     }
 
+    public IPacketBuffer(PacketByteBuf buffer) {
+        this.buffer = buffer;
+    }
+
     public void writeItemStack(IItemStack stack) {
         buffer.writeItemStack(stack.getStack());
+    }
+
+    public void writeDouble(double value) {
+        buffer.writeDouble(value);
+    }
+
+    public void writeFloat(float value) {
+        buffer.writeFloat(value);
+    }
+
+    public void writeBoolean(boolean value) {
+        buffer.writeBoolean(value);
+    }
+
+    public void writeString(String value) {
+        buffer.writeString(value);
+    }
+
+    public double readDouble() {
+        return buffer.readDouble();
+    }
+
+    public float readFloat() {
+        return buffer.readFloat();
+    }
+
+    public boolean readBoolean() {
+        return buffer.readBoolean();
+    }
+
+    public String readString() {
+        return buffer.readString();
+    }
+
+    public IItemStack readItemStack() {
+        return new IItemStack(buffer.readItemStack());
     }
 
 }
