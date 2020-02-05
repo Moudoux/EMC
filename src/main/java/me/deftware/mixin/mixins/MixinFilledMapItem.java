c819badddfdd43d8efd757e495ebcc417afff514
CDAGaming <cstack2011@yahoo.com>
2020-02-05 00:33:24 -0600

    [Change] Added BuriedTreasure structure Locating

---
+++ src/main/java/me/deftware/mixin/mixins/MixinFilledMapItem.java
@@ -0,0 +1,25 @@
+package me.deftware.mixin.mixins;
+
+import me.deftware.client.framework.event.events.EventStructureLocation;
+import me.deftware.client.framework.wrappers.world.IBlockPos;
+import net.minecraft.item.FilledMapItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.world.ServerWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(FilledMapItem.class)
+public class MixinFilledMapItem {
+    @Inject(method = "fillExplorationMap", at = @At("TAIL"))
+    private static void fillMap_after(ServerWorld world, ItemStack stack, CallbackInfo ci) {
+        CompoundTag compoundTag = stack.getTag();
+        if (compoundTag != null && compoundTag.contains("Decorations", 9)) {
+            // Try and Get Decoration X and Z
+            EventStructureLocation event = new EventStructureLocation(new IBlockPos(compoundTag.getDouble("x"), 0, compoundTag.getDouble("z")), EventStructureLocation.StructureType.BuriedTreasure);
+            event.broadcast();
+        }
+    }
+}
