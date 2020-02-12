cd2084f783644ba68013069c6839cdb7ba7b4049
deftware <strumaticjoker2@gmail.com>
2020-02-12 18:21:33 +0100

    Remove prints

--- src/main/java/me/deftware/mixin/mixins/MixinGlStateManager.java
+++ src/main/java/me/deftware/mixin/mixins/MixinGlStateManager.java
@@ -2,7 +2,6 @@ package me.deftware.mixin.mixins;
 
 import com.mojang.blaze3d.platform.FramebufferInfo;
 import com.mojang.blaze3d.platform.GlStateManager;
-import me.deftware.client.framework.main.bootstrap.Bootstrap;
 import org.lwjgl.opengl.GLCapabilities;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,16 +14,12 @@ public class MixinGlStateManager {
 
 	@Inject(method = "initFramebufferSupport", at = @At("RETURN"))
 	private static void initFramebufferSupport(GLCapabilities capabilities, CallbackInfoReturnable<String> ci) {
-		Bootstrap.logger.debug("Replacing DEPTH_ATTACHMENT " + FramebufferInfo.DEPTH_ATTACHMENT + " with 33306");
 		FramebufferInfo.DEPTH_ATTACHMENT = 33306;
 	}
 
 	@ModifyVariable(method = "renderbufferStorage", at = @At("HEAD"), ordinal = 1)
 	private static int renderbufferStorage(int internalFormat) {
-		Bootstrap.logger.debug("Replacing internal format " + internalFormat + " with 35056");
-		internalFormat = 35056;
-
-		return internalFormat;
+		return 35056;
 	}
 
 
