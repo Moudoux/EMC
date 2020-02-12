91f6d797dffc966a89a85d7460b50e024f0b53b1
CDAGaming <cstack2011@yahoo.com>
2020-02-12 06:48:48 -0600

    [Fix] NO @Overwrites

--- src/main/java/me/deftware/mixin/mixins/MixinGlStateManager.java
+++ src/main/java/me/deftware/mixin/mixins/MixinGlStateManager.java
@@ -3,6 +3,7 @@ package me.deftware.mixin.mixins;
 import com.mojang.blaze3d.platform.FramebufferInfo;
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
+import me.deftware.client.framework.main.bootstrap.Bootstrap;
 import org.lwjgl.opengl.ARBFramebufferObject;
 import org.lwjgl.opengl.EXTFramebufferObject;
 import org.lwjgl.opengl.GL30;
@@ -12,40 +13,26 @@ import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(GlStateManager.class)
 public class MixinGlStateManager {
 
-	@Shadow
-	private static GlStateManager.FBOMode fboMode;
-
 	@Inject(method = "initFramebufferSupport", at = @At("RETURN"))
 	private static void initFramebufferSupport(GLCapabilities capabilities, CallbackInfoReturnable<String> ci) {
-		System.out.print("Replacing DEPTH_ATTACHMENT " + FramebufferInfo.DEPTH_ATTACHMENT + " with ");
+		Bootstrap.logger.debug("Replacing DEPTH_ATTACHMENT " + FramebufferInfo.DEPTH_ATTACHMENT + " with 33306");
 		FramebufferInfo.DEPTH_ATTACHMENT = 33306;
-		System.out.println(FramebufferInfo.DEPTH_ATTACHMENT);
 	}
 
-	/**
-	 * @author Deftware
-	 */
-	@Overwrite
-	public static void renderbufferStorage(int target, int internalFormat, int width, int height) {
-		System.out.print("Replacing internal format " + internalFormat + " with ");
+	@ModifyVariable(method = "renderbufferStorage", at = @At("HEAD"), ordinal = 1)
+	private static int renderbufferStorage(int internalFormat) {
+		Bootstrap.logger.debug("Replacing internal format " + internalFormat + " with 35056");
 		internalFormat = 35056;
-		System.out.println(internalFormat);
-		RenderSystem.assertThread(RenderSystem::isOnRenderThreadOrInit);
-		switch(fboMode) {
-			case BASE:
-				GL30.glRenderbufferStorage(target, internalFormat, width, height);
-				break;
-			case ARB:
-				ARBFramebufferObject.glRenderbufferStorage(target, internalFormat, width, height);
-				break;
-			case EXT:
-				EXTFramebufferObject.glRenderbufferStorageEXT(target, internalFormat, width, height);
-		}
+
+		return internalFormat;
 	}
 
 
