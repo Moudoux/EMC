f996a1c470e134686ba2cac10db3e37fe6d91663
deftware <strumaticjoker2@gmail.com>
2020-02-18 18:28:23 +0100

    Fix Block rendering compatability with OptiFine

--- src/main/java/me/deftware/mixin/mixins/MixinOptiFineBlockModelRenderer.java
+++ src/main/java/me/deftware/mixin/mixins/MixinOptiFineBlockModelRenderer.java
@@ -12,19 +12,29 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.world.BlockRenderView;
 import net.minecraftforge.client.model.data.IModelData;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Random;
 
+/**
+ *  OptiFine overwrites hooks used by EMC for block render override, so we need to inject into OptiFine and re-add those hooks
+ * @author Deftware
+ */
 @SuppressWarnings("ALL")
 @Mixin(BlockModelRenderer.class)
 public abstract class MixinOptiFineBlockModelRenderer {
 
+    @Shadow(remap = false)
+    public abstract boolean renderModelSmooth(BlockRenderView world, BakedModel model, BlockState state, BlockPos pos, MatrixStack buffer, VertexConsumer vertexConsumer, boolean cull, Random random, long seed, int overlay, IModelData data);
+
+    @Shadow(remap = false)
+    public abstract boolean renderModelFlat(BlockRenderView view, BakedModel model, BlockState state, BlockPos pos, MatrixStack buffer, VertexConsumer vertexConsumer, boolean cull, Random random, long l, int i, IModelData data);
+
     @Inject(method = "renderModel", at = @At("HEAD"), cancellable = true)
     public void renderModel(BlockRenderView blockRenderView_1, BakedModel bakedModel_1, BlockState blockState_1, BlockPos blockPos_1, MatrixStack matrixStack_1, VertexConsumer vertexConsumer_1, boolean boolean_1, Random random_1, long long_1, int int_1, IModelData data, CallbackInfoReturnable<Boolean> ci) {
-        boolean_1 = false;
         if (blockState_1.getBlock() instanceof FluidBlock) {
             ci.setReturnValue(((boolean) SettingsMap.getValue(SettingsMap.MapKeys.RENDER, "FLUIDS", true)));
         } else {
@@ -37,34 +47,18 @@ public abstract class MixinOptiFineBlockModelRenderer {
         }
     }
 
-    /*
-
-    TODO: Fix these methods:
-
-     @ModifyArg(method = {"renderModel"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/block/BlockModelRenderer;tesselateFlat(Lnet/minecraft/world/ExtendedBlockView;Lnet/minecraft/client/render/model/BakedModel;Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/client/render/BufferBuilder;ZLjava/util/Random;J)Z"))
-    private boolean renderModelFlat1(boolean checkSides) {
-        try {
-            if (SettingsMap.isOverrideMode()) {
-                if (SettingsMap.isOverrideMode()) {
-                    return false;
-                }
-            }
-        } catch (Exception exception) {}
-
-        return checkSides;
+    @Inject(method = "renderModelSmooth", at = @At("RETURN"), remap = false, cancellable = true)
+    public void renderModelSmoothInject(BlockRenderView world, BakedModel model, BlockState state, BlockPos pos, MatrixStack buffer, VertexConsumer vertexConsumer, boolean cull, Random random, long seed, int overlay, IModelData data, CallbackInfoReturnable<Boolean> ci) {
+        if (cull) {
+            ci.setReturnValue(renderModelSmooth(world, model, state, pos, buffer, vertexConsumer, false, random, seed, overlay, data));
+        }
     }
 
-    @ModifyArg(method = {"renderModel"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/block/BlockModelRenderer;tesselateSmooth(Lnet/minecraft/world/ExtendedBlockView;Lnet/minecraft/client/render/model/BakedModel;Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/client/render/BufferBuilder;ZLjava/util/Random;J)Z"))
-    private boolean renderModelSmooth1(boolean checkSides) {
-        try {
-            if (SettingsMap.isOverrideMode()) {
-                return false;
-            }
-        } catch (Exception exception) {}
-
-        return checkSides;
+    @Inject(method = "renderModelFlat", at = @At("HEAD"), remap = false, cancellable = true)
+    public void renderModelFlatInject(BlockRenderView view, BakedModel model, BlockState state, BlockPos pos, MatrixStack buffer, VertexConsumer vertexConsumer, boolean cull, Random random, long l, int i, IModelData data, CallbackInfoReturnable<Boolean> ci) {
+        if (cull) {
+            ci.setReturnValue(renderModelFlat(view, model, state, pos, buffer, vertexConsumer, false, random, l, i, data));
+        }
     }
-
-     */
 
 }
