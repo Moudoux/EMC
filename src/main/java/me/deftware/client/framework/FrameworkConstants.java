bb1c824a09a415688d50bdce489fda1dbb15547c
deftware <strumaticjoker2@gmail.com>
2020-03-19 12:43:51 +0100

    Update MAPPING_SYSTEM

--- src/main/java/me/deftware/client/framework/FrameworkConstants.java
+++ src/main/java/me/deftware/client/framework/FrameworkConstants.java
@@ -4,17 +4,17 @@ public class FrameworkConstants {
 
     public static double VERSION = 13.9;
     public static int PATCH = 6;
-
     public static int SCHEME = 3;
+
     public static MappingsLoader MAPPING_LOADER = MappingsLoader.Fabric;
-    public static MappingSystem MAPPING_SYSTEM = MappingSystem.Yarn;
+    public static MappingSystem MAPPING_SYSTEM = MappingSystem.YarnV2;
 
     public static String toDataString() {
-        return String.format("EMC v%s version %s.%s by Deftware using %s with %s mappings", SCHEME, VERSION, PATCH, MAPPING_LOADER.name(), MAPPING_SYSTEM.name());
+        return String.format("EMC v%s version %s.%s using %s with %s mappings", SCHEME, VERSION, PATCH, MAPPING_LOADER.name(), MAPPING_SYSTEM.name());
     }
 
     public enum MappingSystem {
-        Yarn, MCPConfig, MOJANG
+        Yarn, YarnV2, MCPConfig
     }
 
     public enum MappingsLoader {
