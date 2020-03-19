36469a67b4edd145d43805b9db397bb9d13b24ce
deftware <strumaticjoker2@gmail.com>
2020-03-19 12:39:49 +0100

    Update scheme

--- src/main/java/me/deftware/client/framework/FrameworkConstants.java
+++ src/main/java/me/deftware/client/framework/FrameworkConstants.java
@@ -5,13 +5,12 @@ public class FrameworkConstants {
     public static double VERSION = 13.9;
     public static int PATCH = 6;
 
-    public static int SCHEME = 2;
-    public static String AUTHOR = "Deftware";
+    public static int SCHEME = 3;
     public static MappingsLoader MAPPING_LOADER = MappingsLoader.Fabric;
     public static MappingSystem MAPPING_SYSTEM = MappingSystem.Yarn;
 
     public static String toDataString() {
-        return String.format("EMC v%s version %s.%s by %s using %s with %s mappings", SCHEME, VERSION, PATCH, AUTHOR, MAPPING_LOADER.name(), MAPPING_SYSTEM.name());
+        return String.format("EMC v%s version %s.%s by Deftware using %s with %s mappings", SCHEME, VERSION, PATCH, MAPPING_LOADER.name(), MAPPING_SYSTEM.name());
     }
 
     public enum MappingSystem {
