b02d470e14c85631d270fedd4ab6ef7da2a700ac
deftware <strumaticjoker2@gmail.com>
2020-04-11 22:35:55 +0200

    Add window mixin

---
+++ src/main/java/me/deftware/mixin/mixins/MixinWindow.java
@@ -0,0 +1,30 @@
+package me.deftware.mixin.mixins;
+
+import me.deftware.client.framework.path.OSUtils;
+import net.minecraft.client.util.Window;
+import org.lwjgl.glfw.GLFW;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+/**
+ * Credit to https://github.com/juliand665/retiNO
+ */
+@Mixin(Window.class)
+public abstract class MixinWindow {
+
+	@Redirect(
+			at = @At(
+					value = "INVOKE",
+					target = "Lorg/lwjgl/glfw/GLFW;glfwDefaultWindowHints()V"
+			),
+			method = "<init>"
+	)
+	private void redirectDefaultWindowHints() {
+		if (OSUtils.isMac()) {
+			GLFW.glfwDefaultWindowHints();
+			GLFW.glfwWindowHint(GLFW.GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW.GLFW_FALSE);
+		}
+	}
+
+}
