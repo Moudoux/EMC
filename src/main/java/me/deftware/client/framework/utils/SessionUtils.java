03555392433b951a7b57ea85da0ba8b6e1887e73
CDAGaming <cstack2011@yahoo.com>
2020-06-28 12:28:26 -0700

    [Change] Update Sodium + SessionUtils

--- src/main/java/me/deftware/client/framework/utils/SessionUtils.java
+++ src/main/java/me/deftware/client/framework/utils/SessionUtils.java
@@ -1,16 +1,9 @@
 package me.deftware.client.framework.utils;
 
-import com.mojang.authlib.Agent;
-import com.mojang.authlib.UserAuthentication;
-import com.mojang.authlib.exceptions.AuthenticationException;
-import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
-import com.mojang.authlib.yggdrasil.YggdrasilUserAuthentication;
+import me.deftware.client.framework.utils.session.AuthLibSession;
 import me.deftware.mixin.imp.IMixinMinecraft;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.Session;
-
-import java.net.Proxy;
-import java.util.UUID;
 
 public class SessionUtils {
 
@@ -22,27 +15,13 @@ public class SessionUtils {
      * @return Whether the login was successfull
      */
     public static boolean loginWithPassword(String username, String password) {
-        Session session = null;
-        UserAuthentication auth = new YggdrasilUserAuthentication(
-                new YggdrasilAuthenticationService(Proxy.NO_PROXY, UUID.randomUUID().toString()), Agent.MINECRAFT);
-        auth.setUsername(username);
-        auth.setPassword(password);
-        try {
-            auth.logIn();
-            String userName = auth.getSelectedProfile().getName();
-            UUID playerUUID = auth.getSelectedProfile().getId();
-            String accessToken = auth.getAuthenticatedToken();
-
-            session = new Session(userName, playerUUID.toString(), accessToken,
-                    username.contains("@") ? "mojang" : "legacy");
-
-            ((IMixinMinecraft) MinecraftClient.getInstance()).setSession(session);
-
-            return true;
-        } catch (AuthenticationException e) {
-            e.printStackTrace();
+        AuthLibSession session = new AuthLibSession();
+        session.setCredentials(username, password);
+        if (!session.login()) {
+            return false;
         }
-        return false;
+        session.setSession();
+        return true;
     }
 
     /**
---
+++ src/main/java/me/deftware/client/framework/utils/session/AuthLibSession.java
@@ -0,0 +1,78 @@
+package me.deftware.client.framework.utils.session;
+
+import com.mojang.authlib.Agent;
+import com.mojang.authlib.Environment;
+import com.mojang.authlib.UserAuthentication;
+import com.mojang.authlib.exceptions.AuthenticationException;
+import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
+import com.mojang.authlib.yggdrasil.YggdrasilEnvironment;
+import com.mojang.authlib.yggdrasil.YggdrasilUserAuthentication;
+import me.deftware.mixin.imp.IMixinMinecraft;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.Session;
+
+import java.net.Proxy;
+import java.util.UUID;
+
+/**
+ * Allows modifying and creating sessions
+ */
+public class AuthLibSession {
+
+	private final UserAuthentication userAuthentication;
+	private Session session;
+
+	private AuthLibSession(Environment yggdrasil) {
+		userAuthentication = new YggdrasilUserAuthentication(new YggdrasilAuthenticationService(Proxy.NO_PROXY, UUID.randomUUID().toString()), Agent.MINECRAFT);
+	}
+
+	public AuthLibSession(CustomYggdrasil yggdrasil) {
+		this(yggdrasil.build());
+	}
+
+	public AuthLibSession() {
+		this(YggdrasilEnvironment.PROD);
+	}
+
+	public void setCredentials(String username, String password) {
+		userAuthentication.setUsername(username);
+		userAuthentication.setPassword(password);
+	}
+
+	public void logout() {
+		userAuthentication.logOut();
+	}
+
+	public boolean login() {
+		try {
+			userAuthentication.logIn();
+			return true;
+		} catch (AuthenticationException ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+
+	public boolean loggedIn() {
+		return userAuthentication != null && session != null && userAuthentication.isLoggedIn();
+	}
+
+	public Session buildSession() {
+		if (!userAuthentication.isLoggedIn()) throw new RuntimeException("Cannot create session without logging in!");
+		if (session != null) {
+			return session;
+		}
+		session = new Session(userAuthentication.getSelectedProfile().getName(), userAuthentication.getSelectedProfile().getId().toString(),
+				userAuthentication.getAuthenticatedToken(), userAuthentication.getSelectedProfile().getName().contains("@") ? "mojang" : "legacy");
+		return session;
+	}
+
+	public void setSession() {
+		setSession(buildSession());
+	}
+
+	public void setSession(Session session) {
+		((IMixinMinecraft) MinecraftClient.getInstance()).setSession(buildSession());
+	}
+
+}
