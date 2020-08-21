bf085c4adfb0eb4e21887cf20675b26bc2f3cd26
deftware <strumaticjoker2@gmail.com>
2020-08-21 16:16:54 +0200

    Fix some math

--- src/main/java/me/deftware/client/framework/render/batching/QuadRenderStack.java
+++ src/main/java/me/deftware/client/framework/render/batching/QuadRenderStack.java
@@ -22,7 +22,7 @@ public class QuadRenderStack extends RenderStack<QuadRenderStack> {
 			xx *= getScale();
 			yy *= getScale();
 		}
-		if (shader != null) setupUniforms((float) (x + xx), (float) (y + yy));
+		if (shader != null) setupUniforms((float) x, (float) y, (float) (xx - x), (float) (yy - y));
 		// Draw
 		GL11.glVertex2d(xx, y);
 		GL11.glVertex2d(x, y);
@@ -39,7 +39,7 @@ public class QuadRenderStack extends RenderStack<QuadRenderStack> {
 			xx *= getScale();
 			yy *= getScale();
 		}
-		if (shader != null) setupUniforms(x + xx, y + yy);
+		if (shader != null) setupUniforms(x, y, xx - x, yy - y);
 		// Draw
 		GL11.glVertex2f(xx, y);
 		GL11.glVertex2f(x, y);
@@ -48,10 +48,11 @@ public class QuadRenderStack extends RenderStack<QuadRenderStack> {
 		return this;
 	}
 	
-	public void setupUniforms(float width, float height) {
-		int resolution = GL20.glGetUniformLocation(shader.getProgram(), "resolution");
-		// Y axis is inverted in glsl shaders so we have to pass both the height and screen height
-		GL20.glUniform3f(resolution, width, GuiScreen.getDisplayHeight(), height);
+	public void setupUniforms(float x, float y, float width, float height) {
+		int resolution = GL20.glGetUniformLocation(shader.getProgram(), "resolution"),
+			coordinates = GL20.glGetUniformLocation(shader.getProgram(), "coordinates");
+		GL20.glUniform4f(resolution, width, height, GuiScreen.getDisplayWidth(), GuiScreen.getDisplayHeight());
+		GL20.glUniform2f(coordinates, x, y);
 		GL11.glBegin(GL11.GL_QUADS);
 	}
 
