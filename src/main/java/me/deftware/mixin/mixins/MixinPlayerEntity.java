5245fa9ba57470805effba6c0dbe1fe2b3c60a78
CDAGaming <cstack2011@yahoo.com>
2020-08-27 17:44:06 -0500

    [Fix] Ensure Step height remains consistent

--- src/main/java/me/deftware/mixin/mixins/MixinPlayerEntity.java
+++ src/main/java/me/deftware/mixin/mixins/MixinPlayerEntity.java
@@ -3,6 +3,7 @@ package me.deftware.mixin.mixins;
 import me.deftware.client.framework.event.events.EventSneakingCheck;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -18,6 +19,11 @@ public class MixinPlayerEntity {
             return event.isSneaking();
         }
         return self.isSneaking();
+    }
+
+    @Redirect(method = "adjustMovementForSneaking", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerEntity;stepHeight:F", opcode = 180))
+    private float modifyStepHeight(PlayerEntity self, Vec3d origin) {
+        return self == MinecraftClient.getInstance().player ? 0.6f : self.stepHeight;
     }
 
 }
