c9f9e9aa455c10b2cbf572d4637010e2802d033e
RealIndrit <jia@twinflower.net>
2020-08-30 10:56:19 +0200

    Brain

--- src/main/java/me/deftware/client/framework/event/events/EventSubmergedTransition.java
+++ src/main/java/me/deftware/client/framework/event/events/EventSubmergedTransition.java
@@ -6,7 +6,5 @@ import me.deftware.client.framework.event.Event;
 import net.minecraft.fluid.FluidState;
 
 public class EventSubmergedTransition extends Event {
-    private @Setter @Getter FluidState fluidState;
-
     public EventSubmergedTransition() {}
 }
