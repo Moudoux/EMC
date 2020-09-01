60a965a16065daa724c19dcb3097f9751bd1814f
RealIndrit <jia@twinflower.net>
2020-09-01 15:58:52 +0200

    Added Events

---
+++ src/main/java/me/deftware/client/framework/event/events/EventEntityRender.java
@@ -0,0 +1,19 @@
+package me.deftware.client.framework.event.events;
+
+import lombok.Getter;
+import me.deftware.client.framework.entity.Entity;
+import me.deftware.client.framework.event.Event;
+
+public class EventEntityRender extends Event {
+    private @Getter final Entity entity;
+    private @Getter final double x;
+    private @Getter final double y;
+    private @Getter final double z;
+
+    public EventEntityRender(Entity entity, double x, double y, double z){
+        this.entity = entity;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
---
+++ src/main/java/me/deftware/mixin/mixins/render/MixinEntityRenderDispatcher.java
@@ -0,0 +1,23 @@
+package me.deftware.mixin.mixins.render;
+
+import me.deftware.client.framework.event.events.EventEntityRender;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EntityRenderDispatcher.class)
+public class MixinEntityRenderDispatcher {
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    private void render(Entity entity, double x, double y, double z, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+        EventEntityRender event = new EventEntityRender(me.deftware.client.framework.entity.Entity.newInstance(entity), x, y, z);
+        event.broadcast();
+        if (event.isCanceled()) {
+            ci.cancel();
+        }
+    }
+}
