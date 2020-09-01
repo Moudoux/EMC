542cea29b99cd3496940b07e0c728ef0ad33ebb5
wagyourtail <ruwagray@gmail.com>
2020-09-01 15:05:42 -0700

    sound event

---
+++ src/main/java/me/deftware/mixin/mixins/game/MixinSoundSystem.java
@@ -0,0 +1,35 @@
+package me.deftware.mixin.mixins.game;
+
+import me.deftware.client.framework.chat.ChatMessage;
+import me.deftware.client.framework.event.events.EventSound;
+import net.minecraft.client.sound.*;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SoundSystem.class)
+public class MixinSoundSystem {
+
+    @Shadow
+    @Final
+    private SoundManager loader;
+
+    /*
+    * inject after checks to make sure it's a real sound and is playable,
+    * this also means we don't have to worry about getSoundSet being null.
+    */
+    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/sound/SoundInstance;getVolume()F"), method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", cancellable = true)
+    public void onPlay(SoundInstance instance, CallbackInfo info) {
+        Text soundName = instance.getSoundSet(loader).getSubtitle();
+        EventSound event = new EventSound(instance, soundName == null ? null : new ChatMessage().fromText(soundName));
+        event.broadcast();
+        if (event.isCanceled()) {
+            info.cancel();
+        }
+    }
+
+}
