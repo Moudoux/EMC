daaa7236397fc489cba90ed93f1afd1ee764f8d2
deftware <deftware@aristois.net>
2021-03-20 15:48:25 +0100

    Update EntityRayTrace

--- src/main/java/me/deftware/client/framework/world/ray/EntityRayTrace.java
+++ src/main/java/me/deftware/client/framework/world/ray/EntityRayTrace.java
@@ -3,10 +3,13 @@ package me.deftware.client.framework.world.ray;
 import me.deftware.client.framework.entity.Entity;
 import me.deftware.client.framework.math.vector.Vector3d;
 import me.deftware.client.framework.util.minecraft.EntitySwingResult;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.projectile.ProjectileUtil;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.RaycastContext;
 
 /**
  * @author Deftware
@@ -27,7 +30,7 @@ public class EntityRayTrace extends RayTrace<EntitySwingResult> {
 
         net.minecraft.entity.Entity entity = in.getMinecraftEntity();
 
-        HitResult hitResult = entity.raycast(maxDistance, 1f, false);
+        HitResult hitResult = raycast(start, end, entity);
 
         double distance = maxDistance * maxDistance;
         if (hitResult != null && hitResult.getType() == HitResult.Type.BLOCK)
@@ -45,6 +48,10 @@ public class EntityRayTrace extends RayTrace<EntitySwingResult> {
         }
 
         return null;
+    }
+
+    public HitResult raycast(Vector3d start, Vector3d end, net.minecraft.entity.Entity entity) {
+        return MinecraftClient.getInstance().world.raycast(new RaycastContext(start.getMinecraftVector(), end.getMinecraftVector(), RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.NONE, entity));
     }
 
 }
