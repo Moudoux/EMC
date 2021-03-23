51976526dd3af9040a119fd64769e3c1ceb5e740
deftware <deftware@aristois.net>
2021-03-23 20:18:16 +0100

    Update pixel store attributes

--- src/main/java/me/deftware/client/framework/render/texture/GraphicsUtil.java
+++ src/main/java/me/deftware/client/framework/render/texture/GraphicsUtil.java
@@ -37,8 +37,10 @@ public class GraphicsUtil {
         GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
         GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
 
-        GL11.glPixelStorei(GL11.GL_UNPACK_SKIP_ROWS, 0);
+        // Minecraft modifies these
+        GL11.glPixelStorei(GL11.GL_UNPACK_ROW_LENGTH, 0);
         GL11.glPixelStorei(GL11.GL_UNPACK_SKIP_PIXELS, 0);
+        GL11.glPixelStorei(GL11.GL_UNPACK_SKIP_ROWS, 0);
         GL11.glPixelStorei(GL11.GL_UNPACK_ALIGNMENT, 4);
 
         GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA8, width, height, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, bytebuffer);
