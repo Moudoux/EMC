fd92f8564ad713fd7fc7445809826e578e4313d4
deftware <deftware@aristois.net>
2021-04-01 08:42:54 +0200

    Fix barrier flag

--- src/main/java/me/deftware/mixin/mixins/world/MixinWorldClient.java
+++ src/main/java/me/deftware/mixin/mixins/world/MixinWorldClient.java
@@ -11,18 +11,21 @@ import me.deftware.client.framework.global.GameMap;
 import me.deftware.client.framework.world.classifier.BlockClassifier;
 import me.deftware.mixin.imp.IMixinWorldClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.profiler.Profiler;
 import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.GameMode;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.function.Supplier;
@@ -33,12 +36,19 @@ public class MixinWorldClient implements IMixinWorldClient {
     @Unique
     private final Int2ObjectMap<Entity> entities = new Int2ObjectOpenHashMap<>();
 
-    /*@ModifyVariable(method = "randomBlockDisplayTick(IIIILjava/util/Random;ZLnet/minecraft/util/math/BlockPos$Mutable;)V", at = @At("HEAD"))
-    public boolean randomBlockDisplayTick(boolean spawnBarrierBlocks) {
+    @Redirect(method = "getBlockParticle", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerInteractionManager;getCurrentGameMode()Lnet/minecraft/world/GameMode;", opcode = 180))
+    private GameMode getBlockParticle$getGameMode(ClientPlayerInteractionManager clientPlayerInteractionManager) {
         if (GameMap.INSTANCE.get(GameKeys.FULL_BARRIER_TEXTURE, false))
-            return true;
-        return spawnBarrierBlocks;
-    }*/
+            return GameMode.CREATIVE;
+        return clientPlayerInteractionManager.getCurrentGameMode();
+    }
+
+    @Redirect(method = "getBlockParticle", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getItem()Lnet/minecraft/item/Item;", opcode = 180, ordinal = 0))
+    private Item getBlockParticle$getItem(ItemStack itemStack) {
+        if (GameMap.INSTANCE.get(GameKeys.FULL_BARRIER_TEXTURE, false))
+            return Items.BARRIER;
+        return itemStack.getItem();
+    }
 
     @Inject(method = "<init>(Lnet/minecraft/client/network/ClientPlayNetworkHandler;Lnet/minecraft/client/world/ClientWorld$Properties;Lnet/minecraft/util/registry/RegistryKey;Lnet/minecraft/world/dimension/DimensionType;ILjava/util/function/Supplier;Lnet/minecraft/client/render/WorldRenderer;ZJ)V", at = @At("TAIL"))
     private void onConstructed(ClientPlayNetworkHandler clientPlayNetworkHandler, ClientWorld.Properties properties, RegistryKey<World> registryKey, DimensionType dimensionType, int i, Supplier<Profiler> supplier, WorldRenderer worldRenderer, boolean debugWorld, long seed, CallbackInfo ci) {
