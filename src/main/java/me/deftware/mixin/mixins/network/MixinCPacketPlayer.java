f5f023d4a6ba06bd474b89b73600f96d3b36c9f1
deftware <deftware@aristois.net>
2021-05-13 00:51:04 +0200

    Fix mixin bug

--- src/main/java/me/deftware/mixin/mixins/network/MixinCPacketPlayer.java
+++ src/main/java/me/deftware/mixin/mixins/network/MixinCPacketPlayer.java
@@ -2,18 +2,26 @@ package me.deftware.mixin.mixins.network;
 
 import me.deftware.mixin.imp.IMixinCPacketPlayer;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(PlayerMoveC2SPacket.class)
 public class MixinCPacketPlayer implements IMixinCPacketPlayer {
 
+    @Mutable
+    @Final
     @Shadow
     protected double y;
 
+    @Mutable
+    @Final
     @Shadow
     protected boolean onGround;
 
+    @Mutable
+    @Final
     @Shadow
     protected boolean changePosition;
 
