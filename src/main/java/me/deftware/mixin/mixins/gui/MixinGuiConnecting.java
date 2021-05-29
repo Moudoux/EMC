abaf59238e39962262340b5ebc2e0a615341aa7f
deftware <deftware@aristois.net>
2021-05-29 23:18:09 +0200

    Fix crash

--- src/main/java/me/deftware/mixin/mixins/gui/MixinGuiConnecting.java
+++ src/main/java/me/deftware/mixin/mixins/gui/MixinGuiConnecting.java
@@ -2,7 +2,9 @@ package me.deftware.mixin.mixins.gui;
 
 import me.deftware.client.framework.minecraft.Minecraft;
 import me.deftware.client.framework.util.minecraft.ServerConnectionInfo;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ConnectScreen;
+import net.minecraft.client.network.ServerAddress;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,9 +13,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ConnectScreen.class)
 public class MixinGuiConnecting {
 
-    @Inject(method = "connect", at = @At("HEAD"))
-    private void connect(String ip, int port, CallbackInfo ci) {
-        Minecraft.lastConnectedServer = new ServerConnectionInfo("Server", ip + ":" + port, false);
+    @Inject(method = "connect(Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/network/ServerAddress;)V", at = @At("HEAD"))
+    private void connect(MinecraftClient client, ServerAddress address, CallbackInfo ci) {
+        Minecraft.lastConnectedServer = new ServerConnectionInfo("Server", address.getAddress() + ":" + address.getPort(), false);
     }
 
 }
