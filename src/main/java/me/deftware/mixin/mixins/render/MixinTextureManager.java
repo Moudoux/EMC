ec2d58319c9989114a139edda52a2c3cbd5001aa
deftware <deftware@aristois.net>
2021-05-30 13:34:03 +0200

    Fix MixinTextureManager

--- src/main/java/me/deftware/mixin/mixins/render/MixinTextureManager.java
+++ src/main/java/me/deftware/mixin/mixins/render/MixinTextureManager.java
@@ -1,22 +1,23 @@
 package me.deftware.mixin.mixins.render;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import me.deftware.client.framework.global.GameKeys;
 import me.deftware.client.framework.global.GameMap;
-import net.minecraft.client.texture.TextureManager;
+import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Mixin(TextureManager.class)
+@Mixin(RenderSystem.class)
 public class MixinTextureManager {
 
-    private int syncedCount = 0;
+    private static int syncedCount = 0;
 
-    @ModifyVariable(method = "bindTexture", at = @At("HEAD"))
-    private Identifier bindTexture(Identifier resource) {
+    @ModifyVariable(method = "setShaderTexture(ILnet/minecraft/util/Identifier;)V", remap = false, at = @At("HEAD"))
+    private static Identifier setShaderTexture(Identifier resource) {
         if (GameMap.INSTANCE.get(GameKeys.RAINBOW_ITEM_GLINT, false)) {
-            if (resource.getPath().contains("enchanted_item_glint")) {
+            if (resource.equals(ItemRenderer.ENCHANTED_ITEM_GLINT)) {
                 if (syncedCount > 50) { // One tick as Normal Enchant Glint
                     resource = new Identifier(
                             "emc/enchanted_item_glint_rainbow.png");
