f637c20dac4bdd1b5ff146b2846e6a348342d1c6
deftware <deftware@aristois.net>
2021-06-13 14:37:15 +0200

    Update shaders

--- src/main/java/me/deftware/mixin/mixins/shader/MixinWorldRenderer.java
+++ src/main/java/me/deftware/mixin/mixins/shader/MixinWorldRenderer.java
@@ -15,7 +15,6 @@ import net.minecraft.client.render.*;
 import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
-import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Matrix4f;
 import org.spongepowered.asm.mixin.Final;
@@ -58,6 +57,21 @@ public abstract class MixinWorldRenderer {
             shader.init(MinecraftClient.getInstance(), bufferBuilders.getEntityVertexConsumers());
     }
 
+    @Unique
+    private Shader getShader(Object obj) {
+        for (Shader shader : Shader.SHADERS) {
+            if (shader.isEnabled() && shader.getTargetPredicate().test(obj))
+                return shader;
+        }
+        return null;
+    }
+
+    @Unique
+    private float tickDelta;
+
+    @Unique
+    private Framebuffer targetBuffer;
+
     @Inject(method = "loadEntityOutlineShader", at = @At("RETURN"))
     private void reload(CallbackInfo ci) {
         initShaders();
@@ -68,20 +82,6 @@ public abstract class MixinWorldRenderer {
         for (Shader shader : Shader.SHADERS)
             shader.getShaderEffect().setupDimensions(width, height);
     }
-
-    @Unique
-    private Shader getShader(Object obj) {
-        if (canUseShaders()) {
-            for (Shader shader : Shader.SHADERS) {
-                if (shader.isEnabled() && shader.getTargetPredicate().test(obj))
-                    return shader;
-            }
-        }
-        return null;
-    }
-
-    private float tickDelta;
-    private Framebuffer targetBuffer;
 
     @Inject(method = "render", at = @At("HEAD"))
     private void onRender(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f matrix4f, CallbackInfo ci) {
@@ -110,7 +110,7 @@ public abstract class MixinWorldRenderer {
             for (Shader shader : Shader.SHADERS) {
                 if (shader.isRender()) {
                     targetBuffer = shader.getFramebuffer();
-                    shader.getFramebuffer().draw(this.client.getWindow().getFramebufferWidth(), this.client.getWindow().getFramebufferHeight(), false);
+                    shader.getFramebuffer().draw(this.client.getWindow().getFramebufferWidth(), this.client.getWindow().getFramebufferHeight(), MinecraftClient.IS_SYSTEM_MAC);
                     shader.setRender(false);
                 }
             }
@@ -121,15 +121,17 @@ public abstract class MixinWorldRenderer {
 
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/block/entity/BlockEntityRenderDispatcher;render(Lnet/minecraft/block/entity/BlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;)V", opcode = 180, ordinal = 0))
     private void renderBlocKEntity(BlockEntityRenderDispatcher blockEntityRenderDispatcher, BlockEntity blockEntity, float tickDelta, MatrixStack matrix, VertexConsumerProvider vertexConsumerProvider) {
-        Identifier name = BlockEntityType.getId(blockEntity.getType());
-        if (name != null) {
-            Optional<Block> block = BlockRegistry.INSTANCE.find(name.getPath());
-            if (block.isPresent()) {
-                Shader shader = getShader(block.get());
-                if (shader != null) {
-                    shader.setRender(true);
-                    targetBuffer = shader.getFramebuffer();
-                    vertexConsumerProvider = shader.getOutlineVertexConsumerProvider();
+        if (canUseShaders()) {
+            Identifier name = BlockEntityType.getId(blockEntity.getType());
+            if (name != null) {
+                Optional<Block> block = BlockRegistry.INSTANCE.find(name.getPath());
+                if (block.isPresent()) {
+                    Shader shader = getShader(block.get());
+                    if (shader != null) {
+                        shader.setRender(true);
+                        targetBuffer = shader.getFramebuffer();
+                        vertexConsumerProvider = shader.getOutlineVertexConsumerProvider();
+                    }
                 }
             }
         }
@@ -138,12 +140,14 @@ public abstract class MixinWorldRenderer {
 
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;renderEntity(Lnet/minecraft/entity/Entity;DDDFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;)V", opcode = 180))
     private void doRenderEntity(WorldRenderer worldRenderer, Entity entity, double cameraX, double cameraY, double cameraZ, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers) {
-        me.deftware.client.framework.entity.Entity emcEntity = World.getEntityById(entity.getId());
-        Shader shader = getShader(emcEntity);
-        if (shader != null) {
-            shader.setRender(true);
-            targetBuffer = shader.getFramebuffer();
-            vertexConsumers = shader.getOutlineVertexConsumerProvider();
+        if (canUseShaders()) {
+            me.deftware.client.framework.entity.Entity emcEntity = World.getEntityById(entity.getId());
+            Shader shader = getShader(emcEntity);
+            if (shader != null) {
+                shader.setRender(true);
+                targetBuffer = shader.getFramebuffer();
+                vertexConsumers = shader.getOutlineVertexConsumerProvider();
+            }
         }
         renderEntity(entity, cameraX, cameraY, cameraZ, tickDelta, matrices, vertexConsumers);
     }
@@ -159,6 +163,8 @@ public abstract class MixinWorldRenderer {
                 }
             }
             this.client.getFramebuffer().beginWrite(false);
+        } else {
+            outlineVertexConsumerProvider.draw();
         }
     }
 
