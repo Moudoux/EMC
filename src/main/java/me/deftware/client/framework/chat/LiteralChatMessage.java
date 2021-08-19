10ec6f46c5ddcdfc4bb19c097e0766a53df9bea2
deftware <deftware@aristois.net>
2021-08-19 20:26:16 +0200

    Update LiteralChatMessage

--- src/main/java/me/deftware/client/framework/chat/LiteralChatMessage.java
+++ src/main/java/me/deftware/client/framework/chat/LiteralChatMessage.java
@@ -2,10 +2,14 @@ package me.deftware.client.framework.chat;
 
 import me.deftware.client.framework.chat.style.ChatColors;
 
+import java.util.Arrays;
+
 /**
  * @author Deftware
  */
 public class LiteralChatMessage extends ChatMessage {
+
+	public static final LiteralChatMessage EMPTY = new LiteralChatMessage("");
 
 	public LiteralChatMessage(String text) {
 		this(text, ChatColors.WHITE);
@@ -19,6 +23,12 @@ public class LiteralChatMessage extends ChatMessage {
 
 	public static LiteralChatMessage formatted(String text, Object... params) {
 		return new LiteralChatMessage(String.format(text, params));
+	}
+
+	public static LiteralChatMessage[] of(String... text) {
+		return Arrays.stream(text)
+				.map(s -> s == null || s.isEmpty() ? EMPTY : new LiteralChatMessage(s))
+				.toArray(LiteralChatMessage[]::new);
 	}
 
 }
