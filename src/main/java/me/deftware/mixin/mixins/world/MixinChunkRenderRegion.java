0a5a7908d7faf04d000310a94991360d8ef9ed21
deftware <deftware@aristois.net>
2021-09-17 13:15:01 +0200

    Fix crash

--- src/main/java/me/deftware/mixin/mixins/world/MixinChunkRenderRegion.java
+++ src/main/java/me/deftware/mixin/mixins/world/MixinChunkRenderRegion.java
@@ -6,7 +6,9 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.render.chunk.ChunkRendererRegion;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkSectionPos;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.chunk.WorldChunk;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -17,22 +19,32 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ChunkRendererRegion.class)
 public abstract class MixinChunkRenderRegion {
 
-	@Shadow @Final
-	protected BlockState[] blockStates;
+	@Shadow
+	@Final
+	protected int chunkZOffset;
 
 	@Shadow
-	protected abstract int getIndex(BlockPos pos);
+	@Final
+	protected int chunkXOffset;
+
+	@Shadow
+	@Final
+	protected WorldChunk[][] chunks;
 
 	/*
 		This function is called every time a chunk is built
-	 */
+	*/
 	@Inject(method = "getBlockState", at = @At("HEAD"))
 	private void getBlockState(BlockPos pos, CallbackInfoReturnable<BlockState> state) {
-		if (blockStates != null && pos != null) {
-			BlockState blockState = blockStates[getIndex(pos)];
-			Block block = blockState.getBlock();
-			int id = Registry.BLOCK.getRawId(block);
-			BlockClassifier.getClassifiers().forEach(blockClassifier -> blockClassifier.classify(block, pos, id));
+		if (pos != null) {
+			int i = ChunkSectionPos.getSectionCoord(pos.getX()) - this.chunkXOffset;
+			int j = ChunkSectionPos.getSectionCoord(pos.getZ()) - this.chunkZOffset;
+			BlockState blockState = this.chunks[i][j].getBlockState(pos);
+			if (blockState != null) {
+				Block block = blockState.getBlock();
+				int id = Registry.BLOCK.getRawId(block);
+				BlockClassifier.getClassifiers().forEach(blockClassifier -> blockClassifier.classify(block, pos, id));
+			}
 		}
 	}
 
