0ffad9ed2abbc2ab8230dfeee9fbd1a7ebf4f70a
deftware <deftware@aristois.net>
2022-04-13 00:13:59 +0200

    Use Randomizer

--- src/main/java/me/deftware/client/framework/world/gen/DecoratorConfig.java
+++ src/main/java/me/deftware/client/framework/world/gen/DecoratorConfig.java
@@ -3,13 +3,13 @@ package me.deftware.client.framework.world.gen;
 import lombok.Getter;
 import lombok.Setter;
 import me.deftware.client.framework.world.block.Block;
+import me.deftware.client.framework.world.chunk.Randomizer;
 import me.deftware.mixin.mixins.biome.CountInvoker;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.heightprovider.HeightProvider;
 import net.minecraft.world.gen.placementmodifier.CountPlacementModifier;
 
 import java.util.*;
-import java.util.random.RandomGenerator;
 
 /**
  * Represents Biome ore generation
@@ -80,7 +80,7 @@ public class DecoratorConfig {
         this.feature = feature;
     }
 
-    public int getY(RandomGenerator random, DecoratorContext context) {
+    public int getY(Randomizer random, DecoratorContext context) {
         return this.heightProvider.get((Random) random, context.getHeightContext());
     }
 
@@ -88,7 +88,7 @@ public class DecoratorConfig {
         return feature.ordinal();
     }
 
-    public int getRepeat(RandomGenerator random) {
+    public int getRepeat(Randomizer random) {
         return this.repeat.getInvokedCount((Random) random, null);
     }
 
